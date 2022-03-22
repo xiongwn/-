213310a8d90c9c7c5459683b6da244d3cdd5b53d
xiongwn <pipixia173@gmail.com>
2022-03-22 16:56:22 +0800

    1

--- tool/jtoe.js
+++ tool/jtoe.js
@@ -1,9 +1,11 @@
 var xlsx = require('node-xlsx')
 var fs = require("fs")
 var json = require("./1.js")
+// console.log(json)
 // import * as xlsx from 'node-xlsx'
 // import * as fs from 'fs'
 // import {default as json} from "./1.js"
+json = json.map(i => {i.pay_info = JSON.stringify(i.pay_info);i.extra = JSON.stringify(i.extra);return i})
 var renderData = json.map(item=>Object.keys(json[0]).map(sub=>item[sub]))
 var data = [{name:"sheet1",data:[Object.keys(json[0]),...renderData]}]
 //console.log(data)
